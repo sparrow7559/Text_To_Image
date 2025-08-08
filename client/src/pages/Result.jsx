e6b3bed7407ae190d9fe76e2fbda0f6efed714fd
sparrow7559 <virajgupta2604@gmail.com>
2025-08-08 15:03:15 +0530

    Refactor Result component to manage image state and loading logic

--- client/src/pages/Result.jsx
+++ client/src/pages/Result.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { assets } from "../assets/assets";
 
 const Result = () => {
+  const [image, setImage] = useState(assets.sample_img_1);
+  const [isImageLoaded, setIsImageLoaded] = useState(false);
+
   return (
     <form
       action=""
@@ -9,25 +12,42 @@ const Result = () => {
     >
       <div>
         <div className="relative">
-          <img src={assets.sample_img_1} className="max-w-sm rounded" alt="" />
+          <img src={image} className="max-w-sm rounded" alt="" />
           <span className="absolute bottom-0 left-0 h-1 bg-blue-500 w-full transition-all duration-[10s]" />
         </div>
         <p className="">Loading....</p>
       </div>
 
-      <div className="flex w-full max-w-xl bg-neutral-500 text-white text-sm p-0.5 mt-10 rounded-full">
-        <input
-          type="text"
-          placeholder="Describe what you want to generate"
-          className="flex-1 bg-transparent outline-none ml-8 max-sm:w-20 placeholder-color"
-        />
-        <button
-          className="bg-zinc-900 px-10 sm:px-16 py-3 rounded-full"
-          type="submit"
-        >
-          Generate
-        </button>
-      </div>
+      {!isImageLoaded && (
+        <div className="flex w-full max-w-xl bg-neutral-500 text-white text-sm p-0.5 mt-10 rounded-full">
+          <input
+            type="text"
+            placeholder="Describe what you want to generate"
+            className="flex-1 bg-transparent outline-none ml-8 max-sm:w-20 placeholder-color"
+          />
+          <button
+            className="bg-zinc-900 px-10 sm:px-16 py-3 rounded-full cursor-pointer"
+            type="submit"
+          >
+            Generate
+          </button>
+        </div>
+      )}
+
+      {isImageLoaded && (
+        <div className="flex gap-2 flex-wrap justify-center text-white text-sm p-0.5 mt-10 rounded-full">
+          <p className="bg-transparent border border-zinc-900 text-black px-8 py-3 rounded-full cursor-pointer">
+            Generate Another
+          </p>
+          <a
+            href={image}
+            download
+            className="bg-zinc-900 px-10 py-3 rounded-full cursor-pointer"
+          >
+            Download
+          </a>
+        </div>
+      )}
     </form>
   );
 };
