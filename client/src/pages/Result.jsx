fe9ed2796a06dd745f9110be7d8f43b4900f8bf3
sparrow7559 <virajgupta2604@gmail.com>
2025-08-08 16:07:15 +0530

    Refactor Result component to enhance loading logic and input handling

--- client/src/pages/Result.jsx
+++ client/src/pages/Result.jsx
@@ -4,23 +4,34 @@ import { assets } from "../assets/assets";
 const Result = () => {
   const [image, setImage] = useState(assets.sample_img_1);
   const [isImageLoaded, setIsImageLoaded] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [input, setInput] = useState("");
+
+  const onSubmitHandler = async (e) => {};
 
   return (
     <form
+      onSubmit={onSubmitHandler}
       action=""
       className="flex flex-col min-h-[90vh] justify-center items-center"
     >
       <div>
         <div className="relative">
           <img src={image} className="max-w-sm rounded" alt="" />
-          <span className="absolute bottom-0 left-0 h-1 bg-blue-500 w-full transition-all duration-[10s]" />
+          <span
+            className={`absolute bottom-0 left-0 h-1 bg-blue-500 ${
+              loading ? "w-full transition-all duration-[10s]" : "w-0"
+            }`}
+          />
         </div>
-        <p className="">Loading....</p>
+        <p className={!loading ? "hidden" : ""}>Loading....</p>
       </div>
 
       {!isImageLoaded && (
         <div className="flex w-full max-w-xl bg-neutral-500 text-white text-sm p-0.5 mt-10 rounded-full">
           <input
+            onChange={(e) => setInput(e.target.value)}
+            value={input}
             type="text"
             placeholder="Describe what you want to generate"
             className="flex-1 bg-transparent outline-none ml-8 max-sm:w-20 placeholder-color"
@@ -36,7 +47,12 @@ const Result = () => {
 
       {isImageLoaded && (
         <div className="flex gap-2 flex-wrap justify-center text-white text-sm p-0.5 mt-10 rounded-full">
-          <p className="bg-transparent border border-zinc-900 text-black px-8 py-3 rounded-full cursor-pointer">
+          <p
+            className="bg-transparent border border-zinc-900 text-black px-8 py-3 rounded-full cursor-pointer"
+            onClick={() => {
+              setIsImageLoaded(false);
+            }}
+          >
             Generate Another
           </p>
           <a
