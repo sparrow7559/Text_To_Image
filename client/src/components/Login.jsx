802edff43e2919d4a2818ce6e9723e193d36c409
sparrow7559 <virajgupta2604@gmail.com>
2025-08-08 18:39:03 +0530

    Add Login component and integrate it into the App

---
+++ client/src/components/Login.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { assets } from "../assets/assets";
+
+const Login = () => {
+  return (
+    <div className="absolute top-0 left-0 right-0 bottom-0 z-10 backdrop-blur-sm bg-black/30 flex justify-center items-center">
+      <form
+        action=""
+        className="relative bg-white p-10 rounded-xl text-slate-500"
+      >
+        <h1 className="text-center text-2xl text-neutral-700 font-medium">
+          Sign Up
+        </h1>
+        <p className="text-sm">Welcome back! Please sign in to continue</p>
+        <div className="border px-6 py-2 flex items-center gap-2 rounded-full mt-5">
+          <img src={assets.profile_icon} alt="" width={30} className="" />
+          <input
+            type="text"
+            placeholder="Full Name"
+            required
+            className="outline-none text-sm"
+          />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
