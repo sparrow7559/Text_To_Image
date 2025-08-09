1ab0e4dd4a6b47f086bac8be16d938bc760b6534
sparrow7559 <virajgupta2604@gmail.com>
2025-08-09 21:23:43 +0530

    Integrate AppContext into App and Navbar components for showLogin state management

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { AppContext } from "../context/AppContext";
 
 const Navbar = () => {
-  const { user } = useContext(AppContext);
+  const { user, setShowLogin } = useContext(AppContext);
 
   const navigate = useNavigate();
 
@@ -44,7 +44,10 @@ const Navbar = () => {
             <p className="cursor-pointer" onClick={() => navigate("/buy")}>
               Pricing
             </p>
-            <button className="bg-zinc-800 text-white px-7 py-2 sm:px-10 text-sm rounded-full">
+            <button
+              className="bg-zinc-800 text-white px-7 py-2 sm:px-10 text-sm rounded-full cursor-pointer"
+              onClick={() => setShowLogin(true)}
+            >
               Login
             </button>
           </div>
