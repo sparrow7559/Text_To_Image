625bdfb2cd2f3841b604848d89eead9fba7a25ec
sparrow7559 <virajgupta2604@gmail.com>
2025-08-09 20:59:24 +0530

    Refactor Login component to manage body overflow and enhance state handling

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,8 +1,16 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { assets } from "../assets/assets";
 
 const Login = () => {
   const [state, setState] = useState("Login");
+
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+
+    return () => {
+      document.body.style.overflow = "unset";
+    };
+  }, []);
 
   return (
     <div className="absolute top-0 left-0 right-0 bottom-0 z-10 backdrop-blur-sm bg-black/30 flex justify-center items-center">
@@ -14,15 +22,17 @@ const Login = () => {
           {state}
         </h1>
         <p className="text-sm">Welcome back! Please sign in to continue</p>
-        <div className="border px-6 py-2 flex items-center gap-2 rounded-full mt-4">
-          <img src={assets.profile_icon} alt="" width={30} className="" />
-          <input
-            type="text"
-            placeholder="Full Name"
-            required
-            className="outline-none text-sm"
-          />
-        </div>
+        {state !== "Login" && (
+          <div className="border px-6 py-2 flex items-center gap-2 rounded-full mt-4">
+            <img src={assets.profile_icon} alt="" width={30} className="" />
+            <input
+              type="text"
+              placeholder="Full Name"
+              required
+              className="outline-none text-sm"
+            />
+          </div>
+        )}
         <div className="border px-6 py-2 flex items-center gap-2 rounded-full mt-5">
           <img src={assets.email_icon} alt="" width={20} className="" />
           <input
@@ -46,18 +56,30 @@ const Login = () => {
         </p>
 
         <button className="bg-blue-600 w-full text-white py-2 rounded-full">
-          Create Account
+          {state === "Login" ? "Login" : "Create Account"}
         </button>
 
-        <p className="mt-5 text-center">
-          Don't have an account?{" "}
-          <span className="text-blue-600 cursor-pointer">Sign Up</span>
-        </p>
-
-        <p className="mt-5 text-center">
-          Already have an account?
-          <span className="text-blue-600 cursor-pointer">Login</span>
-        </p>
+        {state === "Login" ? (
+          <p className="mt-5 text-center">
+            Don't have an account?{" "}
+            <span
+              className="text-blue-600 cursor-pointer"
+              onClick={() => setState("Sign Up")}
+            >
+              Sign Up
+            </span>
+          </p>
+        ) : (
+          <p className="mt-5 text-center">
+            Already have an account?
+            <span
+              className="text-blue-600 cursor-pointer"
+              onClick={() => setState("Login")}
+            >
+              Login
+            </span>
+          </p>
+        )}
         <img
           src={assets.cross_icon}
           className="absolute top-5 right-5 cursor-pointer"
