6c672ca0cf577e4ee36768e0b0854fac8196e1dd
sparrow7559 <virajgupta2604@gmail.com>
2025-08-09 21:16:28 +0530

    Add showLogin state management to AppContextProvider

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -4,9 +4,12 @@ export const AppContext = createContext();
 
 const AppContextProvider = (props) => {
   const [user, setUser] = useState(null);
+  const [showLogin, setShowLogin] = useState(false);
   const value = {
     user,
     setUser,
+    showLogin,
+    setShowLogin,
   };
   return (
     <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
