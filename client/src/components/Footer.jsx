e6ee669aded4a0c5ba34f71a8f4c8430c35de273
sparrow7559 <virajgupta2604@gmail.com>
2025-08-10 23:55:44 +0530

    Added animations to credit page and result page and added path to footer image

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import { assets } from "../assets/assets";
+import { useNavigate } from "react-router-dom";
 
 const Footer = () => {
+  const navigate = useNavigate();
   return (
     <div className="flex items-center justify-between gap-4 py-3">
-      <img src={assets.logo} width={150} alt="" className="cursor-pointer" />
+      <img
+        src={assets.logo}
+        width={150}
+        alt=""
+        className="cursor-pointer"
+        onClick={() => navigate("/")}
+      />
       <p className="flex-1 border-l border-gray-400 pl-4 text-sm text-gray-500 max-sm:hidden">
         Copyright @Viraj.dev | All rights reserved
       </p>
