2b8c903dd516f957a7f10da394b26e0b659223c8
sparrow7559 <virajgupta2604@gmail.com>
2025-08-10 20:42:56 +0530

    added animations to header and added login func

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
 import { assets } from "../assets/assets";
 import { motion } from "framer-motion";
+import { AppContext } from "../context/AppContext";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const { user, setShowLogin } = useContext(AppContext);
+  const navigate = useNavigate();
+
+  const onClickHandler = () => {
+    if (user) {
+      navigate("/result");
+    } else {
+      setShowLogin(true);
+    }
+  };
+
   return (
     <motion.div
       className="flex flex-col justify-center items-center"
@@ -37,15 +50,32 @@ const Header = () => {
         Unleash your creativity with AI. Turn your imagination into visual art
         in seconds - just type, and watch the magic happen
       </motion.p>
-      <button className="sm:text-lg text-white bg-black w-auto mt-8 px-12 py-2.5 flex items-center gap-2 rounded-full cursor-pointer">
+      <motion.button
+        className="sm:text-lg text-white bg-black w-auto mt-8 px-12 py-2.5 flex items-center gap-2 rounded-full cursor-pointer"
+        whileHover={{ scale: 1.05 }}
+        whileTap={{ scale: 0.95 }}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{
+          default: { duration: 0.5 },
+          opacity: { delay: 0.8, duration: 1 },
+        }}
+        onClick={onClickHandler}
+      >
         Generate Images <img className="h-6" src={assets.star_group} alt="" />
-      </button>
+      </motion.button>
 
-      <div className="flex flex-wrap justify-center mt-16 gap-3">
+      <motion.div
+        className="flex flex-wrap justify-center mt-16 gap-3"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 1, duration: 1 }}
+      >
         {Array(6)
           .fill("")
           .map((item, index) => (
-            <img
+            <motion.img
+              whileHover={{ scale: 1.05, duration: 0.1 }}
               className="rounded hover:scale-105 transition-all duration-300 cursor-pointer max-sm:w-10"
               src={index % 2 === 0 ? assets.sample_img_2 : assets.sample_img_1}
               alt=""
@@ -53,8 +83,15 @@ const Header = () => {
               width={70}
             />
           ))}
-      </div>
-      <p className="mt-2 text-neutral-600">Generated images from imagify</p>
+      </motion.div>
+      <motion.p
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 1.2, duration: 0.8 }}
+        className="mt-2 text-neutral-600"
+      >
+        Generated images from imagify
+      </motion.p>
     </motion.div>
   );
 };
