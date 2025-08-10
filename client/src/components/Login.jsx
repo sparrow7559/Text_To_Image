2f7e9c83c00bcebfdefecb6b5e688506089c0b8e
sparrow7559 <virajgupta2604@gmail.com>
2025-08-10 23:57:57 +0530

    Added animation to login popup (Frontend finished)

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from "react";
 import { assets } from "../assets/assets";
 import { AppContext } from "../context/AppContext";
+import { motion } from "framer-motion";
 
 const Login = () => {
   const [state, setState] = useState("Login");
@@ -16,7 +17,11 @@ const Login = () => {
 
   return (
     <div className="fixed top-0 left-0 right-0 bottom-0 z-10 backdrop-blur-sm bg-black/30 flex justify-center items-center">
-      <form
+      <motion.form
+        initial={{ opacity: 0.2, y: 50 }}
+        transition={{ duration: 0.3 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true }}
         action=""
         className="relative bg-white p-10 rounded-xl text-slate-500"
       >
@@ -88,7 +93,7 @@ const Login = () => {
           className="absolute top-5 right-5 cursor-pointer"
           alt=""
         />
-      </form>
+      </motion.form>
     </div>
   );
 };
