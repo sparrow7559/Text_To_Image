7e0c748413bd3e23490e1f90fa6a46b19ea908d7
sparrow7559 <virajgupta2604@gmail.com>
2025-08-10 17:45:32 +0530

    Refactor Header component to use motion elements for enhanced animations

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,20 +1,42 @@
 import React from "react";
 import { assets } from "../assets/assets";
+import { motion } from "framer-motion";
 
 const Header = () => {
   return (
-    <div className="flex flex-col justify-center items-center">
-      <div className="text-stone-500 inline-flex text-center gap-2 bg-white px-6 py-1 rounded-full border border-neutral-500">
+    <motion.div
+      className="flex flex-col justify-center items-center"
+      initial={{ opacity: 0.2, y: 100 }}
+      transition={{ duration: 1 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true }}
+    >
+      <motion.div
+        className="text-stone-500 inline-flex text-center gap-2 bg-white px-6 py-1 rounded-full border border-neutral-500"
+        initial={{ opacity: 0, y: -20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.2, duration: 0.8 }}
+      >
         <p className="">Best text to image generator</p>
         <img src={assets.star_icon} alt="" />
-      </div>
-      <h1 className="text-4xl max-w-[300px] sm:text-7xl sm:max-w-[590px] mx-auto mt-10 text-center">
+      </motion.div>
+      <motion.h1
+        className="text-4xl max-w-[300px] sm:text-7xl sm:max-w-[590px] mx-auto mt-10 text-center"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 0.4, duration: 2 }}
+      >
         Turn text to <span className="text-blue-600">image</span>, in seconds.
-      </h1>
-      <p className="text-center max-w-xl mx-auto mt-5">
+      </motion.h1>
+      <motion.p
+        className="text-center max-w-xl mx-auto mt-5"
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.6, duration: 0.8 }}
+      >
         Unleash your creativity with AI. Turn your imagination into visual art
         in seconds - just type, and watch the magic happen
-      </p>
+      </motion.p>
       <button className="sm:text-lg text-white bg-black w-auto mt-8 px-12 py-2.5 flex items-center gap-2 rounded-full cursor-pointer">
         Generate Images <img className="h-6" src={assets.star_group} alt="" />
       </button>
@@ -33,7 +55,7 @@ const Header = () => {
           ))}
       </div>
       <p className="mt-2 text-neutral-600">Generated images from imagify</p>
-    </div>
+    </motion.div>
   );
 };
 
