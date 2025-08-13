11a8bdbae83e18835dc7ea15ccb21ccb499ac887
sparrow7559 <virajgupta2604@gmail.com>
2025-08-13 16:10:33 +0530

    Added path to credit api

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,9 +1,15 @@
-import { registerUser, loginUser } from "../controllers/userControllers.js";
+import {
+  registerUser,
+  loginUser,
+  userCredits,
+} from "../controllers/userControllers.js";
 import express from "express";
+import userAuth from "../middlewares/auth.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/register", registerUser);
 userRouter.post("/login", loginUser);
+userRouter.post("/credits", userAuth, userCredits);
 
 export default userRouter;
