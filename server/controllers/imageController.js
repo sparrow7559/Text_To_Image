d939b3b76779449086020dba5ee56f743ae0ed26
sparrow7559 <virajgupta2604@gmail.com>
2025-08-13 16:43:29 +0530

    added clipdrop api and image conversion. Added routes for image

--- server/controllers/imageController.js
+++ server/controllers/imageController.js
@@ -1,4 +1,6 @@
+import axios from "axios";
 import userModel from "../models/userModel";
+import FormData from "form-data";
 
 export const generateImage = async (req, res) => {
   try {
@@ -16,6 +18,35 @@ export const generateImage = async (req, res) => {
         creditBalance: user.creditBalance,
       });
     }
+
+    const formData = new FormData();
+    formData.append("prompt", prompt);
+
+    const { data } = await axios.post(
+      "https://clipdrop-api.co/text-to-image/v1",
+      formData,
+      {
+        headers: {
+          "x-api-key": process.env.CLIPDROP_API,
+        },
+        responseType: "arraybuffer",
+      }
+    );
+
+    const base64Image = Buffer.from(data, "binary").toString("base64");
+
+    const resultImage = `data:image/png;base64,${base64Image}`;
+
+    await userModel.findByIdAndUpdate(user._id, {
+      creditBalance: user.creditBalance - 1,
+    });
+
+    res.json({
+      success: true,
+      message: "Image Generated",
+      creditBalance: user.creditBalance - 1,
+      resultImage,
+    });
   } catch (error) {
     console.log(error);
     res.json({ success: false, message: error.message });
