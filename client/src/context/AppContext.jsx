dcd81d65a5cd8157a56f06c85fdd7a3c9d5750b3
sparrow7559 <virajgupta2604@gmail.com>
2025-08-13 16:58:48 +0530

    Added backend url to frontend

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -5,11 +5,15 @@ export const AppContext = createContext();
 const AppContextProvider = (props) => {
   const [user, setUser] = useState(null);
   const [showLogin, setShowLogin] = useState(false);
+
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+
   const value = {
     user,
     setUser,
     showLogin,
     setShowLogin,
+    backendUrl,
   };
   return (
     <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
