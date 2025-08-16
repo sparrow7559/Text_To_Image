ed786dcc223a01e67515e4f585b0d42560fb2329
sparrow7559 <virajgupta2604@gmail.com>
2025-08-16 14:39:39 +0530

    Payment verification

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -148,4 +148,36 @@ const paymentRazorpay = async (req, res) => {
   }
 };
 
+const verifyRazorpay = async (req, res) => {
+  try {
+    const { razorpay_order_id } = req.body;
+
+    const orderInfo = await razorpayInstance.order.fetch(razorpay_order_id);
+
+    if (orderInfo.status === "paid") {
+      const transactionData = await transactionModel.findById(
+        orderInfo.receipt
+      );
+      if (transactionData.payment) {
+        return res.json({ success: false, message: "Payment Failed" });
+      }
+      const userData = await userModel.findById(transactionData.userId);
+
+      const creditBalance = userData.creditBalance + transactionData.credits;
+      await userModel.findByIdAndUpdate(userData._id, { creditBalance });
+
+      await transactionModel.findByIdAndUpdate(transactionData._id, {
+        payment: true,
+      });
+
+      res.json({ success: true, message: "Credits Added" });
+    } else {
+      res.json({ success: false, message: "Payment failed" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({ success: falase, message: error.message });
+  }
+};
+
 export { registerUser, loginUser, userCredits, paymentRazorpay };
