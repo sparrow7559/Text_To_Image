7ba4d181df636b092cb915735af91d18d6ec2af9
sparrow7559 <virajgupta2604@gmail.com>
2025-08-16 14:44:12 +0530

    verification connected to frontend

--- client/src/pages/BuyCredit.jsx
+++ client/src/pages/BuyCredit.jsx
@@ -22,7 +22,21 @@ const BuyCredit = () => {
       order_id: order.id,
       receipt: order.receipt,
       handler: async (response) => {
-        console.log(response);
+        try {
+          const { data } = await axios.post(
+            backendUrl + "/api/user/verify-razor",
+            response,
+            { headers: { token } }
+          );
+          if (data.success) {
+            loadCreditsData();
+
+            navigate("/");
+            toast.success("Credits Added");
+          }
+        } catch (error) {
+          toast.error(error.message);
+        }
       },
     };
     const rzp = new window.Razorpay(options);
